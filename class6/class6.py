29333b3b1971ae0aed97aeb3c5f7b24b53d6c8ec
Ethan510huang <ethanhuang510@gmail.com>
2023-11-05 11:00:31 +0800

    class6

---
+++ class6/class6.py
@@ -0,0 +1,39 @@
+# x = 0
+# y = int(input("請輸入money"))
+# while y != 0:
+#     x += y
+#     print("目前總金額為" + str(x) + "元")
+#     y = int(input("請輸入money"))
+
+
+# import time
+
+# y = int(input("請輸入秒數"))
+# for x in range(y, 0, -1):
+#     print(x)
+#     time.sleep(1)
+# else:
+#     print("時間到")
+
+# while True:
+#     print("1 = apple juice 2 = orange juice 3 = grape juice 4 = close")
+#     x = input("請輸入編號")
+#     if x == "1":
+#         print("you chose apple juice")
+#     elif x == "2":
+#         print("you chose orange juice")
+#     elif x == "3":
+#         print("you chose grape juice")
+#     elif x == "4":
+#         print("close")
+#         break
+#     else:
+#         print("error")
+
+
+y = int(input("請輸入次數"))
+for x in range(1, (y + 1)):
+    if x % 10 == 0:
+        print("rest")
+        continue
+    print("第" + str(x) + "次跳繩")
