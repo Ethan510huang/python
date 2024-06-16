c2a0e3419a52e41a09f733ec280b912c92fa2fc2
Ethan510huang <ethanhuang510@gmail.com>
2024-06-16 11:00:36 +0800

    class14

---
+++ 進階/IOT/adv-14/prj01/main.py
@@ -0,0 +1,51 @@
+#########################匯入模組#########################
+import paho.mqtt.client as mqtt
+import time
+import getpass
+import os
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+
+
+#########################函式與類別定義#########################
+def on_publish(client, userdata, mid, reason_code, properties):
+    print(f"Message {mid} has been published.")
+
+
+#########################宣告與設定#########################
+client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
+client.on_publish = on_publish
+client.username_pw_set("singular", "Singular#1234")
+client.connect("mqtt.singularinnovation-ai.com", 1883, 60)
+client.loop_start()
+os.environ["OPENAI_API_KEY"] = getpass.getpass()
+
+
+model = ChatOpenAI(model="gpt-4o", temperature=0.9)
+
+
+#########################主程式#########################
+while True:
+    ans = input("請輸入想跟AI說的話:")  # 輸入訊息
+    msg = model.invoke(
+        [
+            HumanMessage(
+                content="""你是一個負責開關柵欄和開關燈的管理員
+                    'on'代表開燈
+                    'off'代表關燈
+                    '0'代表關柵欄
+                    '90'代表開柵欄
+                    'none'代表不開關
+                    你只能依照使用者的問題回答'on'或'off'或'none'或'1'或'90'或'none'不可以回答他字串"""
+            ),
+            HumanMessage(content=ans),
+        ]
+    ).content
+    print(msg)
+    result = client.publish("owo_AI", msg)  # 發布訊息
+    result.wait_for_publish()
+    if result.rc == mqtt.MQTT_ERR_SUCCESS:
+        print("Message published successfully")
+    else:
+        print("Failed to publish message")
+    time.sleep(0.1)
